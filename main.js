7005b51546c8ab144ac57c35ada68686cbbda621
MADHAN <142477713+madhan007t@users.noreply.github.com>
2024-07-27 11:41:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+ // Typed.js initialization
+ var typed = new Typed(".text", {
+     strings: ["Frontend Developer", "Web Developer"],
+     typeSpeed: 100,
+     backSpeed: 100,
+     backDelay: 1000,
+     loop: true
+ }); 
+ 
+
+
+ 
+ 
+ 
+ 
+ 
